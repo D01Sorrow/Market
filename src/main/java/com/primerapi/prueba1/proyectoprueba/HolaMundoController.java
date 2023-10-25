794e3c57ccd62db2ae3f970f3ffac8214e2ac2f5
D01Sorrow <gabrieldubucv2@gmail.com>
2023-10-25 17:14:51 -0500

    Inicialización del proyecto

---
+++ src/main/java/com/primerapi/prueba1/proyectoprueba/HolaMundoController.java
@@ -0,0 +1,16 @@
+package com.primerapi.prueba1.proyectoprueba;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/saludar")
+public class HolaMundoController {
+
+    @RequestMapping("/hola")
+    public String saludar(){
+
+        return "Nunca pares de aprender";
+    }
+    
+}
---
+++ src/test/java/com/primerapi/prueba1/proyectoprueba/ProyectopruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primerapi.prueba1.proyectoprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectopruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
