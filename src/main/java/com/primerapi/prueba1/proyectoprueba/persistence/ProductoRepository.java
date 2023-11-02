c0206fbb48bf9245490efd8804893ce140bf56f1
D01Sorrow <gabrieldubucv2@gmail.com>
2023-11-02 15:56:12 -0500

    Inyección de dependencia con @Autowired

--- src/main/java/com/primerapi/prueba1/proyectoprueba/persistence/ProductoRepository.java
+++ src/main/java/com/primerapi/prueba1/proyectoprueba/persistence/ProductoRepository.java
@@ -9,12 +9,16 @@ import com.primerapi.prueba1.proyectoprueba.persistence.mapper.ProductMapper;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ProductoRepository implements ProductRepository  {
 
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
+
+    @Autowired
     private ProductMapper mapper;
     
     @Override
@@ -30,28 +34,28 @@ public class ProductoRepository implements ProductRepository  {
 
     @Override
     public Optional<List<Product>> getByCategory(int idCategory) {
-        // TODO Auto-generated method stub
+        
         List<Producto> productos = productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategory);
         return Optional.of(mapper.toProducts(productos));
     }
 
     @Override
     public Optional<List<Product>> getByStock(int stock) {
-        // TODO Auto-generated method stub
+        
         Optional<List<Producto>> productos = productoCrudRepository.findByCantidadStockLessThanAndEstado(stock, true);
         return productos.map(prods -> mapper.toProducts(prods));
     }
 
     @Override
     public Optional<Product> getProduct(int idProduct) {
-        // TODO Auto-generated method stub
+        
         Optional<Producto> producto  = productoCrudRepository.findById(idProduct);
         return producto.map(prod -> mapper.toProduct(prod));
     }
 
     @Override
     public Product save(Product product) {
-        // TODO Auto-generated method stub
+        
         Producto producto = mapper.toProducto(product);
         return mapper.toProduct(productoCrudRepository.save(producto));
     }
